d77e6066efc1c728de843a917e9c47a47e6cff59
Stéphane Depierrepont <toorop@toorop.fr>
2014-05-10 16:38:44 +0200

    Add helper handleError
    Signed-off-by: Stéphane Depierrepont <toorop@toorop.fr>

---
+++ helpers.go
@@ -0,0 +1,19 @@
+package bitcoind
+
+import (
+	"errors"
+	"fmt"
+)
+
+// handleError handle error returned by client.call
+func handleError(err error, r *rpcResponse) error {
+	if err != nil {
+		return err
+	}
+	if r.Err != nil {
+		rr := r.Err.(map[string]interface{})
+		return errors.New(fmt.Sprintf("(%v) %s", rr["code"].(float64), rr["message"].(string)))
+
+	}
+	return nil
+}
