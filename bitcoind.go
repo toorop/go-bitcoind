f72c32796e845e3fa1c8e303964b4f626b11044c
Stéphane Depierrepont <toorop@toorop.fr>
2014-05-19 19:38:18 +0200

    typo
    Signed-off-by: Stéphane Depierrepont <toorop@toorop.fr>

--- bitcoind.go
+++ bitcoind.go
@@ -15,28 +15,28 @@ const (
 )
 
 // A bitpay represents a bitpay client wrapper
-type bitcoind struct {
+type Bitcoind struct {
 	client *rpcClient
 }
 
 // New return a new bitcoind
-func New(host string, port int, user, passwd string, useSSL bool) (*bitcoind, error) {
+func New(host string, port int, user, passwd string, useSSL bool) (*Bitcoind, error) {
 	rpcClient, err := newClient(host, port, user, passwd, useSSL)
 	if err != nil {
 		return nil, err
 	}
-	return &bitcoind{rpcClient}, nil
+	return &Bitcoind{rpcClient}, nil
 }
 
 // BackupWallet Safely copies wallet.dat to destination,
 // which can be a directory or a path with filename on the remote server
-func (b *bitcoind) BackupWallet(destination string) error {
+func (b *Bitcoind) BackupWallet(destination string) error {
 	r, err := b.client.call("backupwallet", []string{destination})
 	return handleError(err, &r)
 }
 
 // DumpPrivKey return private key as string associated to public <address>
-func (b *bitcoind) DumpPrivKey(address string) (privKey string, err error) {
+func (b *Bitcoind) DumpPrivKey(address string) (privKey string, err error) {
 	r, err := b.client.call("dumpprivkey", []string{address})
 	if err = handleError(err, &r); err != nil {
 		return
@@ -46,13 +46,13 @@ func (b *bitcoind) DumpPrivKey(address string) (privKey string, err error) {
 }
 
 // EncryptWallet encrypts the wallet with <passphrase>.
-func (b *bitcoind) EncryptWallet(passphrase string) error {
+func (b *Bitcoind) EncryptWallet(passphrase string) error {
 	r, err := b.client.call("encryptwallet", []string{passphrase})
 	return handleError(err, &r)
 }
 
 // GetAccount returns the account associated with the given address.
-func (b *bitcoind) GetAccount(address string) (account string, err error) {
+func (b *Bitcoind) GetAccount(address string) (account string, err error) {
 	r, err := b.client.call("getaccount", []string{address})
 	if err = handleError(err, &r); err != nil {
 		return
@@ -65,7 +65,7 @@ func (b *bitcoind) GetAccount(address string) (account string, err error) {
 // payments to this account.
 // If account does not exist, it will be created along with an
 // associated new address that will be returned.
-func (b *bitcoind) GetAccountAddress(account string) (address string, err error) {
+func (b *Bitcoind) GetAccountAddress(account string) (address string, err error) {
 	r, err := b.client.call("getaccountaddress", []string{account})
 	if err = handleError(err, &r); err != nil {
 		return
@@ -77,7 +77,7 @@ func (b *bitcoind) GetAccountAddress(account string) (address string, err error)
 // GetBalance return the balance of the server or of a specific account
 //If [account] is "", returns the server's total available balance.
 //If [account] is specified, returns the balance in the account
-func (b *bitcoind) GetBalance(account string, minconf uint64) (balance float64, err error) {
+func (b *Bitcoind) GetBalance(account string, minconf uint64) (balance float64, err error) {
 	r, err := b.client.call("getbalance", []interface{}{account, minconf})
 	if err = handleError(err, &r); err != nil {
 		return
@@ -87,7 +87,7 @@ func (b *bitcoind) GetBalance(account string, minconf uint64) (balance float64, 
 }
 
 // GetBestBlockhash returns the hash of the best (tip) block in the longest block chain.
-func (b *bitcoind) GetBestBlockhash() (bestBlockHash string, err error) {
+func (b *Bitcoind) GetBestBlockhash() (bestBlockHash string, err error) {
 	r, err := b.client.call("getbestblockhash", nil)
 	if err = handleError(err, &r); err != nil {
 		return
@@ -97,7 +97,7 @@ func (b *bitcoind) GetBestBlockhash() (bestBlockHash string, err error) {
 }
 
 // GetBlock returns information about the block with the given hash.
-func (b *bitcoind) GetBlock(blockHash string) (block block, err error) {
+func (b *Bitcoind) GetBlock(blockHash string) (block block, err error) {
 	r, err := b.client.call("getblock", []string{blockHash})
 	if err = handleError(err, &r); err != nil {
 		return
@@ -107,7 +107,7 @@ func (b *bitcoind) GetBlock(blockHash string) (block block, err error) {
 }
 
 // GetBlockCount returns the number of blocks in the longest block chain.
-func (b *bitcoind) GetBlockCount() (count uint64, err error) {
+func (b *Bitcoind) GetBlockCount() (count uint64, err error) {
 	r, err := b.client.call("getblockcount", nil)
 	if err = handleError(err, &r); err != nil {
 		return
@@ -117,7 +117,7 @@ func (b *bitcoind) GetBlockCount() (count uint64, err error) {
 }
 
 // GetBlockHash returns hash of block in best-block-chain at <index>
-func (b *bitcoind) GetBlockHash(index uint64) (hash string, err error) {
+func (b *Bitcoind) GetBlockHash(index uint64) (hash string, err error) {
 	r, err := b.client.call("getblockhash", []uint64{index})
 	if err = handleError(err, &r); err != nil {
 		return
@@ -135,7 +135,7 @@ type getBlockTemplateParams struct {
 // TODO a finir
 // GetBlockTemplate Returns data needed to construct a block to work on.
 // See BIP_0022 for more info on params.
-func (b *bitcoind) GetBlockTemplate(capabilities []string, mode string) (template string, err error) {
+func (b *Bitcoind) GetBlockTemplate(capabilities []string, mode string) (template string, err error) {
 	params := getBlockTemplateParams{
 		Mode:         mode,
 		Capabilities: capabilities,
@@ -150,7 +150,7 @@ func (b *bitcoind) GetBlockTemplate(capabilities []string, mode string) (templat
 }
 
 // GetConnectionCount returns the number of connections to other nodes.
-func (b *bitcoind) GetConnectionCount() (count uint64, err error) {
+func (b *Bitcoind) GetConnectionCount() (count uint64, err error) {
 	r, err := b.client.call("getconnectioncount", nil)
 	if err = handleError(err, &r); err != nil {
 		return
@@ -161,7 +161,7 @@ func (b *bitcoind) GetConnectionCount() (count uint64, err error) {
 
 // GetDifficulty returns the proof-of-work difficulty as a multiple of
 // the minimum difficulty.
-func (b *bitcoind) GetDifficulty() (difficulty float64, err error) {
+func (b *Bitcoind) GetDifficulty() (difficulty float64, err error) {
 	r, err := b.client.call("getdifficulty", nil)
 	if err = handleError(err, &r); err != nil {
 		return
@@ -171,7 +171,7 @@ func (b *bitcoind) GetDifficulty() (difficulty float64, err error) {
 }
 
 // GetInfo return result of "getinfo" command (Amazing !)
-func (b *bitcoind) GetInfo() (i info, err error) {
+func (b *Bitcoind) GetInfo() (i info, err error) {
 	r, err := b.client.call("getinfo", nil)
 	if err = handleError(err, &r); err != nil {
 		return
@@ -181,7 +181,7 @@ func (b *bitcoind) GetInfo() (i info, err error) {
 }
 
 // GetNewAddress return a new address for account [account].
-func (b *bitcoind) GetNewAddress(account ...string) (addr string, err error) {
+func (b *Bitcoind) GetNewAddress(account ...string) (addr string, err error) {
 	// 0 or 1 account
 	if len(account) > 1 {
 		err = errors.New("Bad parameters for GetNewAddress: you can set 0 or 1 account")
@@ -196,7 +196,7 @@ func (b *bitcoind) GetNewAddress(account ...string) (addr string, err error) {
 }
 
 // GetAddressesByAccount return addresses associated with account <account>
-func (b *bitcoind) GetAddressesByAccount(account string) (addresses []string, err error) {
+func (b *Bitcoind) GetAddressesByAccount(account string) (addresses []string, err error) {
 	r, err := b.client.call("getaddressesbyaccount", []string{account})
 	if err = handleError(err, &r); err != nil {
 		return
@@ -206,7 +206,7 @@ func (b *bitcoind) GetAddressesByAccount(account string) (addresses []string, er
 }
 
 // walletPassphrase stores the wallet decryption key in memory for <timeout> seconds.
-func (b *bitcoind) WalletPassphrase(passPhrase string, timeout uint64) error {
+func (b *Bitcoind) WalletPassphrase(passPhrase string, timeout uint64) error {
 	r, err := b.client.call("walletpassphrase", []interface{}{passPhrase, timeout})
 	return handleError(err, &r)
 }
