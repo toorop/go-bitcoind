fea53a5c376aa08332eb4ec94ae00527ae084560
Stéphane Depierrepont <toorop@toorop.fr>
2014-05-21 21:10:54 +0200

    merge rawtransaction to transaction
    Signed-off-by: Stéphane Depierrepont <toorop@toorop.fr>

--- transaction.go
+++ transaction.go
@@ -1,5 +1,50 @@
 package bitcoind
 
+// A ScriptSig represents a scriptsyg
+type ScriptSig struct {
+	Asm string `json:"asm"`
+	Hex string `json:"hex"`
+}
+
+// Vin represent en IN value
+type Vin struct {
+	Coinbase  string    `json:"coinbase"`
+	Txid      string    `json:"txid"`
+	Vout      int       `json:"vout"`
+	ScriptSig ScriptSig `json:"scriptSig"`
+	Sequence  uint32    `json:"sequence"`
+}
+
+type ScriptPubKey struct {
+	Asm       string   `json:"asm"`
+	Hex       string   `json:"hex"`
+	ReqSigs   int      `json:"reqSigs,omitempty"`
+	Type      string   `json:"type"`
+	Addresses []string `json:"addresses,omitempty"`
+}
+
+// Vin represent en OUT value
+type Vout struct {
+	Value        float64      `json:"value"`
+	N            int          `json:"n"`
+	ScriptPubKey ScriptPubKey `json:"scriptPubKey"`
+}
+
+// RawTx represents a raw transaction
+type RawTransaction struct {
+	Hex           string `json:"hex"`
+	Txid          string `json:"txid"`
+	Version       uint32 `json:"version"`
+	LockTime      uint32 `json:"locktime"`
+	Vin           []Vin  `json:"vin"`
+	Vout          []Vout `json:"vout"`
+	BlockHash     string `json:"blockhash,omitempty"`
+	Confirmations uint64 `json:"confirmations,omitempty"`
+	Time          int64  `json:"time,omitempty"`
+	Blocktime     int64  `json:"blocktime,omitempty"`
+}
+
+// TransactionDetails represents details about a transaction
 type TransactionDetails struct {
 	Account  string  `json:"account"`
 	Address  string  `json:"address,omitempty"`
@@ -22,4 +67,25 @@ type Transaction struct {
 	TimeReceived    int64                `json:"timereceived"`
 	Details         []TransactionDetails `json:"details"`
 	Hex             string               `json:"hex"`
+}
+
+// UTransactionOut represents a unspent transaction out (UTXO)
+type UTransactionOut struct {
+	Bestblock     string       `json:"bestblock"`
+	Confirmations uint32       `json:"confirmations"`
+	Value         float64      `json:"value"`
+	ScriptPubKey  ScriptPubKey `json:"scriptPubKey"`
+	Version       uint32       `json:"version"`
+	Coinbase      bool         `json:"coinbase"`
+}
+
+// TransactionOutSet represents statistics about the unspent transaction output database
+type TransactionOutSet struct {
+	Height          uint32  `json:"height"`
+	Bestblock       string  `json:"bestblock"`
+	Transactions    float64 `json:"transactions"`
+	TxOuts          float64 `json:"txouts"`
+	BytesSerialized float64 `json:"bytes_serialized"`
+	HashSerialized  string  `json:"hash_serialized"`
+	TotalAmount     float64 `json:"total_amount"`
 }
