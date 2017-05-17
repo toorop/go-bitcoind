978455e615ce70b228c080bdba4e620c17cbf93d
paulbdavis <paul@dangersalad.com>
2017-05-17 19:32:17 -0400

    actual returns

--- bitcoind.go
+++ bitcoind.go
@@ -41,20 +41,20 @@ func (b *Bitcoind) BackupWallet(destination string) error {
 func (b *Bitcoind) CreateRawTransaction(inputs []*RawTransactionInput, output RawTransactionOutput) (rawtx string, err error) {
 	inStr, err := json.Marshal(inputs)
 	if err != nil {
-		return
+		return "", err
 	}
 	outStr, err := json.Marshal(output)
 	if err != nil {
-		return
+		return "", err
 	}
 
 	r, err := b.client.call("createrawtransaction", []string{string(inStr), string(outStr)})
 	if err = handleError(err, &r); err != nil {
-		return
+		return "", err
 	}
 
 	rawtx = string(r.Result)
-	return
+	return rawtx, nil
 
 }
 
