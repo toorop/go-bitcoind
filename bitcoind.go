840f9a70db2ba808f74cd49ab3e796a0347e5cb6
Chao <200615@gmail.com>
2017-11-21 16:39:00 +0800

    fix listunspent

--- bitcoind.go
+++ bitcoind.go
@@ -554,7 +554,7 @@ func (b *Bitcoind) ListTransactions(account string, count, from uint32) (transac
 }
 
 // ListUnspent returns array of unspent transaction inputs in the wallet.
-func (b *Bitcoind) ListUnspent(minconf, maxconf uint32) (transactions []Transaction, err error) {
+func (b *Bitcoind) ListUnspent(minconf, maxconf uint32) (transactions []ListUnspentResult, err error) {
 	if maxconf > 999999 {
 		maxconf = 999999
 	}
--- result.go
+++ result.go
@@ -12,6 +12,19 @@ type CreateMultiSigResult struct {
 	RedeemScript string `json:"redeemScript"`
 }
 
+// ListUnspentResult models a successful response from the listunspent request.
+type ListUnspentResult struct {
+	TxID          string  `json:"txid"`
+	Vout          uint32  `json:"vout"`
+	Address       string  `json:"address"`
+	Account       string  `json:"account"`
+	ScriptPubKey  string  `json:"scriptPubKey"`
+	RedeemScript  string  `json:"redeemScript,omitempty"`
+	Amount        float64 `json:"amount"`
+	Confirmations int64   `json:"confirmations"`
+	Spendable     bool    `json:"spendable"`
+}
+
 // SignRawTransactionError models the data that contains script verification
 // errors from the signrawtransaction request.
 type SignRawTransactionError struct {
